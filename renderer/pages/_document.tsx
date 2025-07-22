61aacded80a40c92508b5d4dbd21cb1c803f74a1
abhi-yo <akshatsing11@gmail.com>
2025-07-22 20:46:21 +0530

    chore: add DM Sans font import to _document.tsx

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -3,8 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body style={{ margin: 0, padding: 0, backgroundColor: 'transparent' }}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" 
+          rel="stylesheet" 
+        />
+      </Head>
+      <body style={{ 
+        margin: 0, 
+        padding: 0, 
+        backgroundColor: 'transparent',
+        fontFamily: '"DM Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif'
+      }}>
         <Main />
         <NextScript />
       </body>

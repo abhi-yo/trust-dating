d9abaf10e1f089f224ea0d7ee766f82b2ad0824e
abhi-yo <akshatsing11@gmail.com>
2025-08-08 23:39:40 +0530

    feat: remove Google Fonts for fully-local runtime, use system fonts

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -4,18 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" 
-          rel="stylesheet" 
-        />
+        {/* Removed remote Google Fonts; rely on system fonts to keep runtime fully local */}
       </Head>
       <body style={{ 
         margin: 0, 
         padding: 0, 
         backgroundColor: 'transparent',
-        fontFamily: '"DM Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif'
+        fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif'
       }}>
         <Main />
         <NextScript />
